6d48996d4b44eb08d5a641500fe7330f5977a355
John Krupka <john.krupka@united.com>
2024-02-11 11:12:33 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddHealthChecks()
+    .AddCheck("Toggle Check", new ToggleCheck())
+    .AddCheck("Another Check", () =>
+    {
+        Console.WriteLine("another check");
+        return HealthCheckResult.Healthy("simple is good");
+    });
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/health");
+
+app.Run();
+
+public class ToggleCheck : IHealthCheck
+{
+    bool healthy = true;
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("toggle check");
+        healthy = !healthy;
+        return Task.FromResult(healthy ? HealthCheckResult.Healthy("good") : HealthCheckResult.Unhealthy("bad"));
+    }
+}
